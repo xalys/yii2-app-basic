50dbe73d23887c54c923abd7851c6b970e52dc8b
xalys <xalysbek@gmail.com>
2019-08-22 12:13:16 +0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ return [
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
+  
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
